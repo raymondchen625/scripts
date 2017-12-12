c24fc29c7392db2dfd7f926c9ce26ac21bd9f337
Raymond <raymondchen625@gmail.com>
2017-12-12 15:35:47 -0500

    Delete k8s.sh

--- k8s.sh
+++
@@ -1,7 +0,0 @@
-alias d-rmdi='docker images -f dangling=true -q | xargs docker rmi'
-alias d-stopall='docker ps -q | xargs docker stop'
-alias d-rmall='docker ps -qa | xargs docker rm'
-alias d-names='docker ps --format "{{.Names}}"'
-alias d-images='docker images --format "{{.Repository}}"'
-function d-bash() { docker ps -a | grep $1 | awk '{print "docker exec -it " $1 " bash";exit;}'  ; }
-function kurl() { curl http://localhost:8001/api/v1/namespaces/$1 ; }

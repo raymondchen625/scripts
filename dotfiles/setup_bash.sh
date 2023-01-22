6dc7e53e8695caf707b7e0bb3b7f473db367eda2
Raymond Chen <rchen@sonatype.com>
2023-01-21 20:26:35 -0500

    Copy utility bash files

--- dotfiles/setup_bash.sh
+++ dotfiles/setup_bash.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 cp -f ~/scripts/dotfiles/.bashrc ~/
+mkdir -p ~/.local/bin
+cp ~/scripts/bash/* ~/.local/bin/
+apt-get -y update && apt-get install -y git build-essential lsof less sysstat apt-file \
+traceroute netcat dnsutils iproute2 iputils-ping iputils-arping iputils-tracepath \
+iputils-clockdiff wget tcpdump nmap net-tools vim unzip curl  netcat telnet htop hping3 pigz

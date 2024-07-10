9f502844c8899b86c5454325d2332b40a6896df2
Raymond Chen <raymondchen625@gmail.com>
2024-07-10 18:15:17 -0400

    Add install-dev-containers-dotfiles.sh

--- dotfiles/install-dev-containers-dotfiles.sh
+++ dotfiles/install-dev-containers-dotfiles.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Set these in vscode's settings.json
 ###
-# "dotfiles.installCommand": "~/scripts/dotfiles/install-dev-containers-bash.sh",
+# "dotfiles.installCommand": "~/scripts/dotfiles/install-dev-containers-dotfiles.sh",
 # "dotfiles.repository": "https://github.com/raymondchen625/scripts.git",
 # "dotfiles.targetPath": "~/scripts",
 ###
-cp -Rf ~/scripts/dotfiles/.* ~
+rsync -av --exclude '.' --exclude '..' ~/scripts/dotfiles/.* ~
